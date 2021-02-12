8cb0e3a5e03751f8131c3a7c1a34949e2a8aa61d
MSBarbieri <matheussouzabarbieri@gmail.com>
2021-02-12 13:40:05 -0300

    enableee glfw and glm

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,7 +1,10 @@
 #include "engine.hpp"
 
-Engine::Engine()
+Engine::Engine(int width,int height,const char* name)
 {
+    this->_width = width;
+    this->_height = height;
+    this->_name = name;
     std::cout << "started the engine object" << std::endl;
 }
 
@@ -13,4 +16,30 @@ Engine::~Engine()
 {
 
     std::cout << "destroy engine object \n";
+}
+
+void Engine::initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    this->_window = glfwCreateWindow(this->_width, this->_height, "Vulkan", nullptr, nullptr);
+}
+void Engine::mainLoop() {
+    while (!glfwWindowShouldClose(this->_window)) {
+        glfwPollEvents();
+    }
+}
+
+void Engine::cleanup() {
+    glfwDestroyWindow(this->_window);
+
+    glfwTerminate();
+}
+
+void Engine::run() {
+    this->initWindow();
+    this->mainLoop();
+    this->cleanup();
 }
--- engine/engine.hpp
+++ engine/engine.hpp
@@ -1,15 +1,27 @@
 #ifndef ENGINE_H
 #define ENGINE_H
+
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 extern "C" class Engine
 {
-private:
-	/* data */
-public:
-	static void test();
-	Engine(/* args */);
+private: // private data
+	GLFWwindow* _window = nullptr;
+	int _width, _height;
+	const char* _name;
+private: //private methods
+	void initWindow();
+	void mainLoop();
+	void cleanup();
+
+public: // public data
+public: // public methods
+	void test();
+	Engine(int width, int height, const char* name);
 	~Engine();
+
+	void run();
 };
 
 #endif
